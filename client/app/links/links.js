62e005e198f6f49a3559c69e70894673da4d4b3e
JosephSun <bazzkhurshid@yahoo.com>
2015-11-06 17:21:38 -0800

    Added getLinks and put data

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,22 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
+.controller('LinksController', function ($scope, Links ) {
   // Your code here
+  //Links refers to the factory.("Links"...) return in the services js file.
+  console.log("$scope.routes", $scope.routes);
+  $scope.getLinks = Links.getLinks;
+  $scope.data = Links; 
+  $scope["/links"] = Links.linksRoute; 
+  $scope.getLinks();
+  
+  $scope.links = function () {
+      Links.linksRoute($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.shortly', token);
+          $location.path('/links');
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    };
 });
